7b175d7b13041178d2ac69be57bd33bc137fe708
m-sherrick <michael.a.sherrick@gmail.com>
2025-02-28 13:33:58 -0800

    Remove test.py for new file to play environment in PyGame

--- Test.py
+++
@@ -1,6 +0,0 @@
-from TetrisEnv import TetrisPyEnv
-from tf_agents.environments import tf_py_environment
-
-test_env = TetrisPyEnv(queue_size=5, seed=123)
-
-tf_env = tf_py_environment.TFPyEnvironment(test_env)
