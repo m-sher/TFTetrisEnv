5d49f58157ad8370e67ff937d7828e03a09d95d5
m-sherrick <michael.a.sherrick@gmail.com>
2025-03-02 04:17:37 -0800

    Testing pyenv

--- PlayPyGame.py
+++ PlayPyGame.py
@@ -10,21 +10,29 @@ os.environ['TF_FORCE_GPU_ALLOW_GROWTH'] = 'true'
 # tf_agents.system.multiprocessing.enable_interactive_mode()
 
 if __name__ == '__main__':
-    constructors = [lambda: TetrisPyEnv(queue_size=5, seed=123) for _ in range(4)]
-    ppy_env = ParallelPyEnvironment(constructors, start_serially=False, blocking=False)
+    # constructors = [lambda: TetrisPyEnv(queue_size=5, seed=123) for _ in range(4)]
+    # ppy_env = ParallelPyEnvironment(constructors, start_serially=False, blocking=False)
     # tf_env = TFPyEnvironment(ppy_env)
     
-    timestep = ppy_env.reset()
+    env = TetrisPyEnv(queue_size=5, seed=1234)
+    
+    timestep = env.reset()
     print(timestep)
 
-    while not any(timestep.is_last()):
-        for row in timestep.observation['board'][0]:
+    while not np.any(timestep.is_last()):
+        for row in timestep.observation['board']:
             print(row)
-        print(timestep.observation['pieces'][0])
+        print(timestep.observation['pieces'])
         action = [int(choice) for choice in input("\nMove: ").strip().split(',')]
+        # action = {
+        #     'hold': np.array([action[0] for _ in range(4)]),
+        #     'standard': np.array([action[1] for _ in range(4)]),
+        #     'spin': np.array([action[2] for _ in range(4)])
+        # }
         action = {
-            'hold': np.array([action[0] for _ in range(4)]),
-            'standard': np.array([action[1] for _ in range(4)]),
-            'spin': np.array([action[2] for _ in range(4)])
+            'hold': action[0],
+            'standard': action[1],
+            'spin': action[2]
         }
-        timestep = ppy_env.step(action)
+        timestep = env.step(action)
+        print(timestep.reward)
