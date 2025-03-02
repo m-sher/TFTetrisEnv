24ecd5352ea54ef738dfd22c5b342bf826f41468
m-sherrick <michael.a.sherrick@gmail.com>
2025-03-02 04:17:19 -0800

    Add delta attributes to pieces

--- Pieces.py
+++ Pieces.py
@@ -15,7 +15,14 @@ class Piece:
     def __init__(self, piece_type: PieceType = PieceType.N,
                  loc: np.ndarray = np.zeros((2,), dtype=np.int32),
                  r: int = 0, cells: np.ndarray = np.zeros((4, 2), dtype=np.int32)):
+        
         self.piece_type = piece_type
+        
         self.loc = loc
+        self.delta_loc = np.zeros((2,), np.int32)
+        
         self.r = r
+        self.delta_r = 0
+        
         self.cells = cells
+        
