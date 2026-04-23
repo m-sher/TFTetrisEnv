fe5392f219008f8daf808a735386dc20fd100854
Michael Sherrick <michael.a.sherrick@gmail.com>
2026-04-22 18:30:46 -0700

    Start uv swap + ignore .so

--- setup.py
+++ setup.py
@@ -1,40 +1,22 @@
-from setuptools import setup, find_packages, Extension
-
-pathfinder_module = Extension(
-    "TetrisEnv.pathfinder",
-    sources=["TetrisEnv/pathfinder.c"],
-    extra_compile_args=["-O3", "-std=c99"],
-)
-
-hole_finder_module = Extension(
-    "TetrisEnv.hole_finder",
-    sources=["TetrisEnv/hole_finder.c"],
-    extra_compile_args=["-O3", "-std=c99"],
-)
-
-b2b_search_module = Extension(
-    "TetrisEnv.b2b_search",
-    sources=["TetrisEnv/b2b_search.c"],
-    extra_compile_args=["-O3", "-std=c99", "-lm"],
-)
+from setuptools import setup, Extension
 
 setup(
-    name="TetrisEnv",
-    version="0.8.0",
-    description="Environment for RL with Tetris SRS+",
-    author="Michael Sherrick",
-    author_email="michael.a.sherrick@gmail.com",
-    url="https://github.com/m-sher/TFTetrisEnv",
-    packages=find_packages(),
-    ext_modules=[pathfinder_module, hole_finder_module, b2b_search_module],
-    install_requires=[
-        "tensorflow",
-        "tf-agents",
-        "numpy",
+    ext_modules=[
+        Extension(
+            "TetrisEnv.pathfinder",
+            sources=["TetrisEnv/pathfinder.c"],
+            extra_compile_args=["-O3", "-std=c99"],
+        ),
+        Extension(
+            "TetrisEnv.hole_finder",
+            sources=["TetrisEnv/hole_finder.c"],
+            extra_compile_args=["-O3", "-std=c99"],
+        ),
+        Extension(
+            "TetrisEnv.b2b_search",
+            sources=["TetrisEnv/b2b_search.c"],
+            extra_compile_args=["-O3", "-std=c99"],
+            extra_link_args=["-lm"],
+        ),
     ],
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "Operating System :: OS Independent",
-    ],
-    python_requires=">=3.6",
 )
